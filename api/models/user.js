33aa55309a2add59c24a88959876b63c98d8222a
Pedro Quadros de Freitas <pedroqdefreitas@gmail.com>
2025-10-20 00:00:30 -0300

    Adding routes and testing database reaction

--- api/models/user.js
+++ api/models/user.js
@@ -4,11 +4,13 @@ const UserSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
+        unique: true
     },
 
     password: {
         type: String,
         required: true,
+        select: false
     },
 });
 
